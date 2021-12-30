c921632b9dd3b5b08723659700e718a002d33ac7
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-12-29 23:54:40 -0800

    Implement select-commit

---
+++ src/cmd/select-commit.ts
@@ -0,0 +1,23 @@
+import chalk from 'chalk';
+import simpleGit, {DefaultLogFields} from 'simple-git';
+
+import {fzfSelect} from '../fzf';
+
+export default async function selectCommit() {
+  const commits = await simpleGit().log({from: 'HEAD', to: 'origin/master'});
+
+  const selected = await fzfSelect<DefaultLogFields>({
+    prompt: 'Select commit(s) for PR:',
+    genValues: addOption =>
+      commits.all.forEach(commit => {
+        const shortHash = commit.hash.slice(0, 8);
+        const label = chalk`{red ${shortHash}} {blue [${commit.author_name}]} {white ${commit.message}}`;
+
+        addOption({label, id: commit.hash, ...commit});
+      }),
+  });
+
+  const commitHashes = selected.map(commit => commit.hash);
+
+  console.log(commitHashes.join('\n'));
+}
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@
 import yargs from 'yargs';
 
 import pr from './cmd/pr';
+import selectCommit from './cmd/select-commit';
 
 yargs(process.argv.slice(2))
   .command('pr', 'Create and update PRs', pr)
+  .command('select-commit', 'Select a commit hash', selectCommit)
   .demandCommand(1, '')
   .parse();
