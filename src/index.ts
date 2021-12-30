a3be762b09e003b7a63743139d36cb83194815ca
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-12-30 00:37:56 -0800

    Auto-set color

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import yargs from 'yargs';
 
 import pr from './cmd/pr';
 import selectCommit from './cmd/select-commit';
 
 yargs(process.argv.slice(2))
+  .option('color', {
+    boolean: true,
+    desc: 'Use colored output (default: auto-detect)',
+  })
+  .middleware(args => {
+    if (args.color !== undefined) {
+      chalk.level = args.color ? 3 : 0;
+    }
+  }, true)
   .command('pr', 'Create and update PRs', pr)
   .command('select-commit', 'Select a commit hash', selectCommit)
   .demandCommand(1, '')
